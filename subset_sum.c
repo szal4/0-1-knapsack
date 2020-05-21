a16c3b02bd9f50482e1fec1b6793ee063b16b9a8
Sajal Jain <59515346+szal4@users.noreply.github.com>
2020-05-21 15:53:25 +0530

    Create subset_sum.c
    to find whether any subset of arr can make the given sum by summing its elements

---
+++ subset_sum.c
@@ -0,0 +1,38 @@
+//to find whether any subset of arr can make the given sum by summing its elements
+#include<stdio.h>
+#include<stdbool.h>
+bool dp[10][25];//according to constrain
+int max(int a,int b){
+    if(a>b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
+bool subset_sum(int *arr,int n,int s){
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<s+1;j++){
+            if(i==0){
+                dp[i][j]=false;
+            }
+            else if(j==0){
+                dp[i][j]=true;
+            }
+        else if(arr[i-1]<=j){
+            dp[i][j]=dp[i-1][j-arr[i-1]]||dp[i-1][j];
+        }
+        else{
+            dp[i][j]=dp[i-1][j];
+        }
+        }
+    }
+    return dp[n][s];
+}
+int main(){
+    int arr[5]={2,3,7,8,10};//given array
+    int s=6;//to make the sum=s
+    int ans=subset_sum(arr,5,s);
+    printf("%d",ans);
+    return 0;
+}
