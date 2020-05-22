0a661852b4bfe256e7e30bf58da3a045f34a221e
Sajal Jain <59515346+szal4@users.noreply.github.com>
2020-05-22 11:33:17 +0530

    Update subset_sum(recursion).c

--- subset_sum(recursion).c
+++ subset_sum(recursion).c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include<stdbool.h>
 bool subset_sum(int arr[],int n,int s){
-    if(n==0){
+    if(n==0&&s!=0){
       return false;
     }
     if(s==0){
@@ -11,13 +11,13 @@ bool subset_sum(int arr[],int n,int s){
         return subset_sum(arr,n-1,s);
     }
     else{
-       return subset_sum(arr,n-1,s-arr[n])||subset_sum(arr,n-1,s); 
+       return subset_sum(arr,n-1,s-arr[n-1])||subset_sum(arr,n-1,s); 
     }
 }
 int main()
 {
    int arr[5]={3,5,8,9,4};
-   int sum=10;
+   int sum=3;
    bool ans=subset_sum(arr,5,sum);
    printf("%d",ans);
     return 0;
